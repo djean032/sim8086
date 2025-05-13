2c60711af002ff62a53e8412ec379a0a92e9e9ff
djean032 <djean032@gmail.com>
2025-05-13 10:04:49 -0400

    changed decoding schem to lookup table

--- sim8086.cpp
+++ sim8086.cpp
@@ -9,6 +9,75 @@
 
 typedef uint8_t u8;
 
+typedef enum op_type {
+    OP_MOV,
+    OP_PUSH,
+    OP_POP,
+} op_type;
+
+typedef enum usage {
+    BITS_END,
+    OPCODE,
+    MOD,
+    REG,
+    RM,
+    SR,
+    W,
+    S,
+    D,
+    V,
+    Z,
+    DATA8,
+    DATALO,
+    DATAHI,
+    DISPLO,
+    DISPHI,
+    IPLO,
+    IPHI,
+    CSLO,
+    CSHI,
+    IPINC8,
+    IPINCLO,
+    IPINCHI,
+    ADDRLO,
+    ADDRHI,
+    XXX,
+    YYY,
+    REG8,
+    REG16,
+    MEM8,
+    MEM16,
+    IMMED8,
+    IMMED16,
+    SEGREG,
+    DESTSTR8,
+    SRCSTR8,
+    DESTSTR16,
+    SRCSTR16,
+    SHORTLBL,
+    NEARPROC,
+    FARPROC,
+    NEARLBL,
+    FARLBL,
+    SRCLBL,
+    ESCOP,
+    SRC,
+} usage;
+
+typedef struct bits {
+    usage bit_use;
+    u8 bit_count;
+    u8 shift;
+    u8 value;
+} bits;
+
+typedef struct opcode {
+    op_type name;
+    bits bits[16];
+} opcode;
+
+
+
 char registers[][2][3] = {
     {"al", "ax"},
     {"cl", "cx"},
@@ -20,13 +89,19 @@ char registers[][2][3] = {
     {"bh", "di"},
 };
 
-char opcodes[][5] = {
-    "mov",
+opcode op_table[7] = {
+    { OP_MOV, { { OPCODE, 6, 0, 0b100010 } } },
+    { OP_MOV, { { OPCODE, 7, 0, 0b1100011 } } },
+    { OP_MOV, { { OPCODE, 4, 0, 0b1011 } } },
+    { OP_MOV, { { OPCODE, 7, 0, 0b1010000 } } },
+    { OP_MOV, { { OPCODE, 7, 0, 0b1010001 } } },
+    { OP_MOV, { { OPCODE, 8, 0, 0b10001110 } } },
+    { OP_MOV, { { OPCODE, 8, 0, 0b10001100 } } },
 };
-
 
 void run_program(size_t program_size, u8 *memory, FILE *test_file);
 void decode_opcode(u8 *memory, size_t *stack_point, FILE *test_file);
+size_t lookup_op(u8 value);
 
 int main(int argc, char **argv) {
 
@@ -63,26 +138,31 @@ void run_program(const size_t program_size, u8 *memory, FILE *test_file) {
     }
 }
 
+size_t lookup_op(u8 value) {
+    size_t found_idx;
+    for (int i = 0; i < (sizeof(op_table)/sizeof(op_table[0])); i++) {
+        if (value == op_table[i].bits[0].value) {
+            found_idx = i;
+            return found_idx;
+        }
+    }
+    return 18;
+}
+
 void decode_opcode(u8 *memory, size_t *stack_point, FILE *test_file) {
-    int op_idx;
-    int op_val = (memory[*stack_point] & 0xFF) >> 2;
-    int w = memory[*stack_point] & 1;
-    int d = (memory[*stack_point] >> 1) & 1;
-    switch (op_val) {
-        case 0x22:
-            op_idx = 0;
-            break;
-        default:
-            printf("Error: Invalid Opcode");
-            exit(1);
+    for (int i = 0; i < 8; i++) {
+        u8 tmp_op;
+        u8 mask = 0xFF;
+        u8 tmp_val = memory[*stack_point] & mask;
+        tmp_op = tmp_val >> i;
+        printf("Current Value: %d\n", tmp_op);
+        size_t found = lookup_op(tmp_op);
+        if (found != 18) {
+            printf("%d\n", op_table[found].bits[0].bit_use);
+        } else {
+            printf("Not Found!\n");
+        }
     }
     (*stack_point)++;
-
-    // Second byte
-    int mod = (memory[*stack_point] >> 6) & 0x3;
-    int reg = (memory[*stack_point] >> 3) & 0x7;
-    int rm = memory[*stack_point] & 0x7;
-
-    fprintf(test_file, "%s %s, %s\n", opcodes[0], registers[d ? reg : rm][w], registers[d ? rm : reg][w]);
-    (*stack_point)++;
 }
+
